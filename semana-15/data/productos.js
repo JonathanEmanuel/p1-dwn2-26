500d76f8281f3bf8b7420f3483e839472ba5ad1c
Jonathan Cruz <cruzjonathan.e@gmail.com>
2026-06-23 21:18:39 -0300

    Semana 16 - Filtrados

--- semana-15/data/productos.js
+++ semana-15/data/productos.js
@@ -1,7 +1,37 @@
- const productos = [
-    { id:1, nombre: 'Mouse 2', descripcion: 'Detalle del Producto', precio: 45000, foto: 'images/mouse.jpg'},
-    { id:2, nombre: 'Mouse Logitech', descripcion: 'Detalle del Producto', precio: 75000, foto: 'images/mouse.jpg'},
-    { id:3, nombre: 'Monitor', descripcion: 'Detalle del Producto', precio: 12000, foto: 'images/monitor.jpg'},
-    { id:4, nombre: 'Teclado', descripcion: 'Detalle del Producto', precio: 65000, foto: 'images/teclado.jpg'},
-    { id:5, nombre: 'Teclado Logitech', descripcion: 'Detalle del Producto', precio: 100, foto: 'images/teclado.jpg'}
+const productos = [
+    { 
+        id: 1, 
+        nombre: 'Mouse 2', 
+        descripcion: 'Detalle del Producto', 
+        precio: 45000, 
+        foto: 'images/mouse.jpg' 
+    },
+    { 
+        id: 2, 
+        nombre: 'Mouse Logitech', 
+        descripcion: 'Detalle del Producto', 
+        precio: 75000, 
+        foto: 'images/mouse.jpg' 
+    },
+    { 
+        id: 3, 
+        nombre: 'Monitor', 
+        descripcion: 'Detalle del Producto', 
+        precio: 12000, 
+        foto: 'images/monitor.jpg' 
+    },
+    { 
+        id: 4, 
+        nombre: 'Teclado mecanico', 
+        descripcion: 'Detalle del Producto, es un teclado RGB con teclas de alto rendimiento', 
+        precio: 65000, 
+        foto: 'images/teclado.jpg' 
+    },
+    { 
+        id: 5, 
+        nombre: 'Teclado Logitech', 
+        descripcion: 'Detalle del Producto', 
+        precio: 100, 
+        foto: 'images/teclado.jpg' 
+    }
 ];
